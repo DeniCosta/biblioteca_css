e1cb456e531eb86de73ed0ae0d125144d4b7c01a
DeniCosta <denisecosta.codes@gmail.com>
2023-08-30 16:16:55 -0300

    Feat: Adiciona as bibliotecas Inquirer, cli-table3 e chalk

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import figlet from "figlet";
 import gradient from "gradient-string";
+import inquirer from "inquirer";
+import cliTable from "cli-table3";
+import chalk from "chalk";
 
 function exibirTitulo() {
   console.log(
